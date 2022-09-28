cf4d4da79105471d884a11259e07ae012d153508
Elias Ferreira Junior <eliaswebdev@gmail.com>
2022-09-28 15:24:13 -0300

    Crud básico de produtos

---
+++ db/migrate/20220928133504_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.float :width
+      t.float :height
+      t.float :weight
+
+      t.timestamps
+    end
+  end
+end
